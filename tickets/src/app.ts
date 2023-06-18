ca6b8ad7346f4b4e839873c9f630af3df3c02d75
AnmZ <anmit.ray911@gmail.com>
2023-06-18 20:44:20 +0530

    wrote crud for ticketService and refactored a lot of code :D

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import 'express-async-errors'
 import {json} from 'body-parser'
-import { errorHandler,NotFoundError } from '@artickit/common';
+import { createTicketRouter } from './routes/new';
 import cookieSession from 'cookie-session';
-
+import { errorHandler,NotFoundError,currentUser} from '@artickit/common';
+import { ShowTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
 
 
 
@@ -15,11 +18,14 @@ app.use(cookieSession({
     secure: true,
 
 }))
-
-
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(ShowTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.get('*',async()=>{
-    throw new NotFoundError();
+    throw new NotFoundError()
 })
 app.use(errorHandler);
 
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,15 +1,26 @@
 
-import {app} from './app';
+import mongoose from 'mongoose';
+
+import { app } from './app';
 
 const startDB = async () => {
     if(!process.env.JWT_KEY)
       {
         throw new Error('JWT NOT DEFINED') ;
       }
+      if(!process.env.MONGO_URI)
+      {
+        throw new Error('MONGO_URI NOT DEFINED')
+      }
       
-    
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+    console.log('db connected');
+    } catch (error) {
+        console.log(error);
+    }
     app.listen(3000,()=>{
-    console.log('Ticket Server Stared at Port 3000');
+    console.log('Ticketing Server Stared at Port 3000');
     })
  
 };
