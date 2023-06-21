03ef3223d79b62bc24193176a4356ddacfae3dc7
AnmZ <anmit.ray911@gmail.com>
2023-06-21 20:09:12 +0530

    added orders-status-enum : ORDER Model

--- orders/routes/new.ts
+++ orders/routes/new.ts
@@ -1,8 +1,19 @@
+import mongoose from 'mongoose';
 import express , {Request,Response} from 'express'
+import { requireAuth,validateRequest} from '@artickit/common';
+import {body} from 'express-validator'
 
 const router = express.Router();
 
-router.post('/api/orders',async(req:Request,res:Response)=>{
+router.post('/api/orders',requireAuth,[
+    body('tickerId')
+    .not()
+    .isEmpty()
+    .custom((input: string)=>mongoose.Types.ObjectId.isValid(input))
+    .withMessage('Ticket Id must be provided')
+    
+
+],validateRequest,async(req:Request,res:Response)=>{
 
     res.send({});
 })
