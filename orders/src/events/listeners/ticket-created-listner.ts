e90d71f5a9258487de18f93e9f2d6235c166d0cd
AnmZ <anmit.ray911@gmail.com>
2023-06-22 19:35:12 +0530

    setup listeners

---
+++ orders/src/events/listeners/ticket-created-listner.ts
@@ -0,0 +1,20 @@
+import { Message } from "node-nats-streaming";
+import { Listener, Subjects, TicketCreatedInterface} from "@artickit/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./utils/queue-group-name";
+
+export class TicketCreatedListener extends Listener<TicketCreatedInterface> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+    queueGroupName = queueGroupName;
+    async onMessage(data: TicketCreatedInterface['data'],msg: Message)
+    {
+        const {id,title,price} = data;
+        const ticket = Ticket.build({
+           id,
+           title,
+           price
+        });
+        await ticket.save();
+        msg.ack();
+    }
+} 
