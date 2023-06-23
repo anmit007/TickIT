38fbd37a94a5d82cca7baed0f339205f85549ba7
AnmZ <anmit.ray911@gmail.com>
2023-06-23 11:57:10 +0530

    added orderId property to TicketSchema: TicketModel

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -11,6 +11,7 @@ interface TicketDoc extends mongoose.Document {
     price: number,
     userId : string,
     version : number,
+    orderId?: string,
 
 }
 
@@ -30,6 +31,9 @@ const ticketSchema = new mongoose.Schema({
     userId: {
         type: String,
         required : true
+    },
+    orderId: {
+        type: String
     } 
 },{
     toJSON: {
