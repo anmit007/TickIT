bf8feaea33b3f1fdd875e4eb9dfd17f28763e99c
AnmZ <anmit.ray911@gmail.com>
2023-06-24 20:18:50 +0530

    coupled charge and order

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -3,6 +3,7 @@ import express , {Request,Response} from "express";
 import { body } from "express-validator";
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
 
 const router = express.Router();
 
@@ -33,14 +34,18 @@ router.post('/api/payments',requireAuth,[
 
     }
 
-    await stripe.charges.create({
+     const charge = await stripe.charges.create({
         currency: 'usd',
         amount: (order.price)*100,
         source: token,
         description: "Ticket Bought"
        
     });
-    
+    const payment = Payment.build({
+        orderId,
+        stripeId: charge.id,
+    })
+    await payment.save();
     res.status(201).send({suceess:true});
 
 });
