b37a43c24056da5fa42ecdce456304dfdf46d6df
AnmZ <anmit.ray911@gmail.com>
2023-06-24 20:54:12 +0530

    added listners for payment create

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,24 @@
+import { Subjects,Listener,PaymentCreatedEvent, OrderStatus, NotFoundError } from "@artickit/common";
+import { queueGroupName } from "./utils/queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent>{
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+    queueGroupName= queueGroupName;
+    async onMessage(data:PaymentCreatedEvent['data'],msg:Message){
+
+    const order = await  Order.findById(data.orderId);
+    if(!order)
+    {
+        throw new NotFoundError();
+    }
+
+    order.set({
+        status: OrderStatus.Complete
+    });
+
+    await order.save();
+    msg.ack();
+    }
+}
