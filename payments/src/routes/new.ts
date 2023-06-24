ae6b3f13c3d0c058023a20c7141da5ef73d17093
AnmZ <anmit.ray911@gmail.com>
2023-06-24 11:36:09 +0530

    added more logic to new route

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,4 +1,4 @@
-import { BadRequestError, NotFoundError, requireAuth, validateRequest } from "@artickit/common";
+import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@artickit/common";
 import express , {Request,Response} from "express";
 import { body } from "express-validator";
 import { Order } from "../models/order";
@@ -13,7 +13,23 @@ router.post('/api/payments',requireAuth,[
     .isEmpty(),
     body('orderId')
     .not().isEmpty().withMessage('Provide a valid orderID')
-],(req:Request,res:Response)=>{
+],async(req:Request,res:Response)=>{
+
+    const {token,orderId} = req.body;
+    const order = await Order.findById(orderId);
+    if(!order) {
+        throw new NotFoundError();
+    }
+    if(order.userId!=req.currentUser!.id)
+    {
+        throw new NotAuthorizedError();
+    }
+
+    if(order.status === OrderStatus.Cancelled)
+    {
+        throw new BadRequestError('Order is cancelled');
+
+    }
 
     res.send({suceess:true});
 
